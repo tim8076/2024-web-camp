71b8e5f1a71e3e7dfd5baa0f598dcd51259fbcbb
shosetim <shosetim32@gmail.com>
2024-05-12 11:11:31 +0800

    增加手機選單

--- source/js/all.js
+++ source/js/all.js
@@ -1,7 +1,25 @@
-const aaa = 'aaa'
+const header = document.querySelector('[data-header]');
+const menuBtn = document.querySelector('[data-menu-button]');
+const menu = document.querySelector('[data-menu-phone]');
 
-const car = () => {
-  console.log('aaa')
-}
+//  漢堡選單切換
+menuBtn.addEventListener('click', function(e) {
+  e.preventDefault();
+  if (e.target.innerText === 'menu') {
+    e.target.innerText = 'close';
+  } else {
+    e.target.innerText = 'menu';
+  }
+  menu.classList.toggle('showMenu');
+  document.querySelector('body').classList.toggle('bodyFixed');
+})
 
-car();
+// // 卷軸滑動時，header變成半透明
+// document.addEventListener('scroll', function() {
+//   let scrollPosition = window.scrollY;
+//   if (scrollPosition > 0) {
+//     header.classList.add('opacity-75');
+//   } else {
+//     header.classList.remove('opacity-75');
+//   }
+// })
